d00db18ffd068e9ac03dd8c6ad30074ebf76f233
divyamjha <divyamjha.70055594@gmail.com>
2026-03-05 16:27:42 +0530

    memo

--- recoil/src/App.jsx
+++ recoil/src/App.jsx
@@ -1,5 +1,8 @@
 // context API
 
+import { useEffect } from "react";
+import { useState, memo } from "react";
+import count from "./store/atom/counter";
 
 // import { useContext } from "react";
 // import { createContext, useState } from "react";
@@ -73,63 +76,116 @@
 
 // export default App;
 
-
 ////////////////////////////////////////////////////////////
 
 // RECOIL
 
-import { useState } from "react";
-import { RecoilRoot, useSetRecoilState, useRecoilValue } from 'recoil';
-import count  from './store/atom/counter.js';
+// import { useState } from "react";
+// import { RecoilRoot, useSetRecoilState, useRecoilValue } from 'recoil';
+// import count  from './store/atom/counter.js';
 
+// const Parent = () => {
+//   return <div>
+//     <RecoilRoot>
+//       <Increase />
+//       <Decrease />
+//       <Value />
+//     </RecoilRoot>
+//   </div>
+// }
 
-const Parent = () => {
-  return <div>
-    <RecoilRoot>
-      <Increase />
-      <Decrease />
-      <Value />
-    </RecoilRoot>
-  </div>
-}
+// function Increase() {
+//   const setCount = useSetRecoilState(count);
 
-function Increase() {
-  const setCount = useSetRecoilState(count);
+//   return <>
+//     <button onClick={() => {
+//       setCount(count => count + 1);
+//     }} >
+//       Increase
+//     </button>
+//   </>
+// }
 
-  return <>
-    <button onClick={() => {
-      setCount(count => count + 1);
-    }} >
-      Increase
-    </button>
-  </>
-}
+// function Decrease() {
+//   const setCount = useSetRecoilState(count);
 
-function Decrease() {
-  const setCount = useSetRecoilState(count);
+//   return <>
+//     <button onClick={() => {
+//       setCount(count => count - 1);
+//     }} >
+//       Decrease
+//     </button>
+//   </>
+// }
 
-  return <>
-    <button onClick={() => {
-      setCount(count => count - 1);
-    }} >
-      Decrease
-    </button>
-  </>
-}
+// function Value() {
+//   const countValue = useRecoilValue(count);
 
+//   return <p>
+//     count: {countValue}
+//   </p>
+// }
 
-function Value() {
-  const countValue = useRecoilValue(count);
+// export default function App() {
+//   return (
+//       <div>
+//        <Parent />
+//       </div>
+//     )
+// }
 
-  return <p>
-    count: {countValue}
-  </p>
-}
+//////////////////////////////////////////////////////////
+
+// memo
 
 export default function App() {
   return (
-      <div>
-       <Parent />
-      </div>
-    )
+    <div>
+      <Counter />
+    </div>
+  );
 }
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Increase setCount={setCount} />
+      <Decrease setCount={setCount} />
+      <Value count={count} setCount={setCount} />
+    </>
+  );
+}
+
+const Increase = memo(function ({ setCount }) {
+  return (
+    <button
+      onClick={() => {
+        setCount((count) => count + 1);
+      }}
+    >
+      Increase
+    </button>
+  );
+});
+
+const Decrease = memo(function ({ setCount }) {
+  return (
+    <button
+      onClick={() => {
+        setCount((count) => count - 1);
+      }}
+    >
+      Decrease
+    </button>
+  );
+});
+
+const Value = memo(function ({ count }) {
+  return (
+    <>
+      <p>Count: {count}</p>
+    </>
+  );
+});
